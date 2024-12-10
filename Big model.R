3973f468f31d2b9758f7d49e25da49b39dc76469
Joseph Anderson <32866693+JosephJohnAnderson@users.noreply.github.com>
2024-12-10 12:09:42 +0100

    Added data combination code
    Added data combination code

--- Big model.R
+++ Big model.R
@@ -1,9 +1,14 @@
 # Load the other datasets
 library(readxl)
-SKS_ABIN <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SKS/AFO data corrected.xlsx", 
+SKS_ABIN <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SKS/SKS_ABIN.xlsx", 
                        sheet = "Data")
 
 MMA_weather <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SMHI/AFO_weather_data.xlsx")
+
+Moose_density <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Moose/Täthet vinterstam 2013-2024, med missing data för enstaka år i främst vargområden, kompletterad.xlsx", 
+                            sheet = "Täthet vinterstam")
+
+## Combine the datasets ####
 
 # Mutate MMA_weather "winter_season" column
 library(dplyr)
@@ -11,21 +16,23 @@ library(stringr)
 MMA_weather <- MMA_weather %>%
   mutate(InvAr = str_sub(winter_season, start = 6))
 
+# Mutate MMA_weather "ÄFO-id" column
+Moose_density <- Moose_density %>%
+  mutate(Registreri = str_sub(`ÄFO-id`, start = 5))
+
 # Set InvAr to numeric
 MMA_weather$InvAr <- as.numeric(MMA_weather$InvAr)
 
-# Associate data sheets based on "Registreri" and "InvAr"
-
+# Add weather data to big data set
 Big_data <- SKS_ABIN %>%
   left_join(MMA_weather, by = c("Registreri", "InvAr"))
 
-# Create the base plot
-plot(Big_data$`mean_seasonal_snowdepth[cm]` ~ Big_data$ArsskadaTallAndel,
-     xlab = "ArsskadaTallAndel",
-     ylab = "Mean Seasonal Snow Depth [cm]",
-     main = "Relationship between Snow Depth and Damage Proportion")
+# Add moose densities to big data set
+Big_data <- Big_data %>%
+  left_join(Moose_density, by = c("Registreri", "InvAr" = "År"))
 
-# Fit the GLMM
+
+## GLMM with big data ####
 library(lme4)
 glmm_model <- lmer(`mean_seasonal_snowdepth[cm]` ~ ArsskadaTallAndel + (1 | Registreri), 
                    data = Big_data)
--- RASE model.R
+++ RASE model.R
@@ -1,8 +1,38 @@
-## Prerequisites ####
-library(readr)
-library(lmer)
+# Load the other datasets
+library(readxl)
+SKS_ABIN <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SKS/SKS_ABIN.xlsx", 
+                       sheet = "Data")
 
-# Import the data ####
-bif_data <- 
+MMA_weather <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SMHI/AFO_weather_data.xlsx")
 
-## GLMM ####
+Moose_density <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Moose/Täthet vinterstam 2013-2024, med missing data för enstaka år i främst vargområden, kompletterad.xlsx", 
+                            sheet = "Täthet vinterstam")
+
+## Combine the datasets ####
+
+# Mutate MMA_weather "winter_season" column
+library(dplyr)
+library(stringr)
+MMA_weather <- MMA_weather %>%
+  mutate(InvAr = str_sub(winter_season, start = 6))
+
+# Mutate MMA_weather "ÄFO-id" column
+Moose_density <- Moose_density %>%
+  mutate(Registreri = str_sub(`ÄFO-id`, start = 5))
+
+# Set InvAr to numeric
+MMA_weather$InvAr <- as.numeric(MMA_weather$InvAr)
+
+# Add weather data to big data set
+Big_data <- SKS_ABIN %>%
+  left_join(MMA_weather, by = c("Registreri", "InvAr"))
+
+# Add moose densities to big data set
+Big_data <- Big_data %>%
+  left_join(Moose_density, by = c("Registreri", "InvAr" = "År"))
+
+
+## GLMM with big data ####
+library(lme4)
+glmm_model <- lmer(`mean_seasonal_snowdepth[cm]` ~ ArsskadaTallAndel + (1 | Registreri), 
+                   data = Big_data)

a6b381706bb6b171ebbc5da74e2eeffa56013b25
Joseph Anderson <32866693+JosephJohnAnderson@users.noreply.github.com>
2025-01-14 10:51:09 +0100

    Updated data reading and combining
    Updated data reading and combining and built first model (GLM)

--- RASE model.R
+++ RASE model.R
@@ -1,44 +1,168 @@
-# Load the other datasets
-library(readxl)
-SKS_ABIN <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SKS/SKS_ABIN.xlsx", 
-                       sheet = "Data")
-
-MMA_weather <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SMHI/AFO_weather_data.xlsx")
-
-Moose_density <- read_excel("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Moose/Täthet vinterstam 2013-2024, med missing data för enstaka år i främst vargområden, kompletterad.xlsx", 
-                            sheet = "Täthet vinterstam")
-
-## Combine the datasets ####
-
-# Mutate MMA_weather "winter_season" column
+# Pre-requisites
+library(openxlsx)
+library(tidyr)
 library(dplyr)
 library(stringr)
-MMA_weather <- MMA_weather %>%
-  mutate(InvAr = str_sub(winter_season, start = 6))
 
-# Mutate MMA_weather "ÄFO-id" column
-Moose_density <- Moose_density %>%
-  mutate(Registreri = str_sub(`ÄFO-id`, start = 5))
+## Import data ####
 
-# Set InvAr to numeric
-MMA_weather$InvAr <- as.numeric(MMA_weather$InvAr)
+# ABIN data
+SKS_ABIN <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SKS/SKS_ABIN.xlsx", 
+                      sheet = "Data")
+
+# SMHI data
+Weather <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/SMHI/MMA_full_weather_data_13_01_2025.xlsx")
+
+    # Set InvAr to numeric
+      Weather$InvAr <- as.numeric(Weather$InvAr)
+
+# Moose density
+Moose_density <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Moose/kjell_moose_data_250105.xlsx")
+
+    # Mutate Moose_density "ÄFO-id" column
+      Moose_density <- Moose_density %>%
+      mutate(Registreri = str_sub(`ÄFO-id`, start = 5))
+
+# Wolf predation
+Wolf_predation <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Moose/Täthet vinterstam 2013-2024, kompletterad, KLrev.xlsx", 
+                            sheet = "Täthet vinterstam")
+
+    # Extract the 2nd and 17th columns as a data frame
+      Wolf_predation <- as.data.frame(Wolf_predation[, c(2, 5, 17)])
+
+    # Rename columns
+      colnames(Wolf_predation) <- c("Registreri", "InvAr", "Wolf_predation")
+
+Wolf_predation$Registreri <- substr(Wolf_predation$Registreri, start = 5, stop = nchar(Wolf_predation$Registreri))
+
+
+# Bear observations
+Bear_obs <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/BearObs.xlsx",)
+
+    # Mutate Moose_density "ÄFO-id" column
+      Bear_obs <- Bear_obs %>%
+      mutate(Registreri = str_sub(`ÄFO`, start = 5))
+
+# Other ungulates
+Ungulate_data <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/HuntingBags/RoeFallowRedWildBAllYear.xlsx", sheet = "AllYears")
+
+    # Mutate ungulate_data "MMA" column
+      Ungulate_data <- Ungulate_data %>%
+      mutate(Registreri = str_sub(`MMA`))
+
+# Calves per female      
+Calves_per_female <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Calves per female and proportion of males 2013-2023/Kalv per hondjur och Andel tjur 2013-2023 Nuvarande gränser.xlsx", sheet = "Kalv per hondjur")
+
+    # Pivot dataframe to have year data as column with corresponding measurement as separate columns
+      Calves_per_female <- Calves_per_female %>%
+      pivot_longer(cols = starts_with("Kalvar"),  # Columns with years
+               names_to = "Year",         # New column for year
+               values_to = "calves_per_female") # New column for measurement values
+
+
+    # Mutate ungulate_data "Älgförvaltningsområde" column
+      Calves_per_female$Registreri <- substr(Calves_per_female$Älgförvaltningsområde, start = 5, stop = nchar(Calves_per_female$Älgförvaltningsområde))
+      Calves_per_female$Year <- substr(Calves_per_female$Year, start = 20, stop = nchar(Calves_per_female$Year))
+      
+    # Convert Year to numeric  
+      Calves_per_female <- Calves_per_female %>%
+      mutate(Year = as.numeric(Year))
+      
+# Sex ratio
+Males_proportion <- read.xlsx("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Calves per female and proportion of males 2013-2023/Kalv per hondjur och Andel tjur 2013-2023 Nuvarande gränser.xlsx", sheet = "Andel tjur")
+
+    # Pivot dataframe to have year data as column with corresponding measurement as separate columns
+      Males_proportion <- Males_proportion %>%
+      pivot_longer(cols = starts_with("Andel"),  # Columns with years
+               names_to = "Year",         # New column for year
+               values_to = "Males_proportion") # New column for measurement values
+      as.numeric(Males_proportion$Year)
+
+    # Mutate "Älgförvaltningsområde" column
+      Males_proportion$Registreri <- substr(Males_proportion$Älgförvaltningsområde, start = 5, stop = nchar(Males_proportion$Älgförvaltningsområde))
+      Males_proportion$Year <- substr(Males_proportion$Year, start = 24, stop = nchar(Males_proportion$Year))
+  
+    # Convert Year to numeric
+      Males_proportion <- Males_proportion %>%
+      mutate(Year = as.numeric(Year))
+      
+      
+# Calf weights
+# data in wrong format for big data, would need one one average weighted values per AFO  to be merged with rest of data  
+Calf_weights <- read.csv("//storage-um.slu.se/restricted$/vfm/Vilt-Skog/Moose-Targets/Calf weights/CalfWeights_AFO_Season_MeanWeight_Count_WeightedWeight20241217.csv")
+  
+    # Mutate "InvAr" column
+      Calf_weights <- Calf_weights %>%
+      mutate(InvAr = str_sub(season, start = 6)) # use for season end year
+    
+    # Convert "InvAr" to numeric
+      Calf_weights <- Calf_weights %>%
+      mutate(InvAr = as.numeric(InvAr))
+     
+      # Calf_weights <- Calf_weights %>%
+      # mutate(InvAr = str_sub(season, end = 4)) # use for season start year
+      
+    # Mutate Calf_weights "Area_ID" column
+      Calf_weights <- Calf_weights %>%
+      mutate(Registreri = str_sub(`Area_ID`))
+     
+
+## Join data ####
 
 # Add weather data to big data set
 Big_data <- SKS_ABIN %>%
-  left_join(MMA_weather, by = c("Registreri", "InvAr"))
+  left_join(Weather, by = c("Registreri", "InvAr"))
 
-# Add moose densities to big data set
+# Add moose densities
 Big_data <- Big_data %>%
-  left_join(Moose_density, by = c("Registreri", "InvAr" = "År"))
+  left_join(Moose_density, by = c("Registreri", "InvAr" = "ÄBINår"))
 
+# Add wolf predation
+Big_data <- Big_data %>%
+  left_join(Wolf_predation, by = c("Registreri", "InvAr"))
 
-## betareg with big data ####
+# Add bear observations 
+Big_data <- Big_data %>%
+  left_join(Bear_obs, by = c("Registreri", "InvAr" = "År"))
+
+# Add calves weights
+Big_data <- Big_data %>%
+  left_join(Calf_weights, by = c("Registreri", "InvAr"))
+
+# Add calves per female
+Big_data <- Big_data %>%
+  left_join(Calves_per_female, by = c("Registreri", "InvAr" = "Year"))
+
+# Add "sex ratio"
+Big_data <- Big_data %>%
+  left_join(Males_proportion, by = c("Registreri", "InvAr" = "Year"))
+
+# Add other ungulates
+Big_data <- Big_data %>%
+  left_join(Ungulate_data, by = c("Registreri", "InvAr" = "Year"))
+
+## Generalised linear model with big data ####
+
+# RASE per hectare GLM
+library(lme4)
+RASE_Ha_glm <- glmer(AntalRASEHa ~ scale(Älgtäthet.i.vinterstam) + scale(FD1000) + scale(AntalTallarHa) + 
+                          scale(AntalGranarHa) + scale(Roe1000) + scale(AndelMargraMarker) + (1 | InvAr) + (1 | Registreri), 
+                          family = poisson, data = Big_data, na.action = na.exclude)
+summary(RASE_Ha_model)
+
+# RASE per hectare Beta regression
 library(betareg)
+RASE_Ha_betar <- betareg(scale(AntalRASEHa) ~ scale(varaible1) + scale(varaible2) + scale(varaible3) + 
+                          scale(varaible4) + scale(varaible5) + scale(varaible6) +
+                          scale(mvaraible7), data = Big_data, na.action=na.exclude)
+summary(RASE_Ha_betar)
 
-# RASE per hectare
-RASE_Ha_model <- betareg(AntalRASEHa ~ scale(varaible1) + scale(varaible2) + scale(varaible3) + 
-                           scale(varaible4) + scale(varaible5) + scale(varaible6) +
-                           scale(mvaraible7), data = Big_data, na.action=na.exclude)
+# RASE per hectare GLM
+library(lme4)
+RASE_Ha_model <- glmer(AntalRASEHa ~ scale(Älgtäthet.i.vinterstam) + scale(FD1000) + scale(AntalTallarHa) + 
+                          scale(AntalGranarHa) + scale(Roe1000) + scale(AndelMargraMarker) + (1 | InvAr) + (1 | Registreri), 
+                          family = poisson, data = Big_data, na.action = na.exclude)
+summary(RASE_Ha_model)
 
 # RASE at competitive height
 RASE_competative_model <- betareg(RASEAndelGynnsam ~ scale(varaible1) + scale(varaible2) + scale(varaible3) + 
